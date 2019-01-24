311724abdbbf7c6073df8e4afaf57490e4e14fa6
Yaroslav <yaroslav.khudchenko@gmail.com>
2019-01-24 12:49:42 +0100

    detect which element was dropped

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $('ol').draggable();
 $('.grid-item').droppable({
   drop: function(e) {
    console.log('drop!');
+   console.log(e);
+   console.log(e.originalEvent.target);
    console.log(e.target);
   }
 });
