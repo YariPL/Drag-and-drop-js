e80c3a26303e4665641464b657c40b45b7cca7a2
Yaroslav <yaroslav.khudchenko@gmail.com>
2019-01-24 15:57:01 +0100

    working correctly adding photos to grids, saving to localstorage,and grabing them from local storage after reload + after dropping original element go back to original place

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 if(window.localStorage.grids){
-	alert('ss')
-	window.onload = function() {
 		var grids = JSON.parse(window.localStorage.getItem("grids"));
-		console.log(grids);
-		console.log(grids.length);
+		
 		for (let i =0 ; i<grids.length;i++) {
 			$('#grid-container > div').eq(i).find("img").attr("src", grids[i]);
 		}
-	}
+
+	console.log(grids);
+		console.log(grids.length);
 } else {
 	var grids = [];
 	grids.length = 9;
@@ -20,21 +19,31 @@ console.log(grids.length);
 $('ol').draggable();
 $('#grid-container > div').droppable({
   drop: function(e) {
- /* console.log('drop!');
+  console.log('drop!');
    console.log(e);
    console.log(e.originalEvent.target);
-   console.log(e.target);*/
+   console.log(e.target);
+   console.log('------------');
+   console.log(grids);
 
+   var ev = e.originalEvent;
 
 	var id = $(this).attr("data-id") * 1;
-	grids[id] = e.originalEvent.target.src ? e.originalEvent.target.src : e.originalEvent.target.querySelector('img').src;
-	$(this).html('<img src="'+grids[id]+'" alt="image:)">');
-	$(e.originalEvent.target).css({
-        top: "0px",
-        left: "0px"
-    });
-    e.originalEvent.target.style.top = '0px';
-    e.originalEvent.target.style.left = '0px';
+	grids[id] = ev.target.src ? e.originalEvent.target.src : e.originalEvent.target.querySelector('img').src;
+	$(this).html('<img src="'+grids[id]+'" alt="image">');
+	
+    console.log(e.originalEvent.target);
+    console.log(e.originalEvent.originalEvent.target.parentElement);
+
+    if(e.originalEvent.target.localName == 'img') {
+
+	   e.originalEvent.originalEvent.target.parentElement.style.top = '0px';
+	   e.originalEvent.originalEvent.target.parentElement.style.left = '0px';
+
+	} else {
+		e.originalEvent.target.style.top = '0px';
+	    e.originalEvent.target.style.left = '0px';
+	}
   }
 });
 
