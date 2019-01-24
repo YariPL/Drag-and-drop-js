5d025dcc34d19ef0d80b1d5e146a47b7040b7027
Yaroslav <yaroslav.khudchenko@gmail.com>
2019-01-24 16:44:46 +0100

    added delete buttons for all the grids

--- app.js
+++ app.js
@@ -1,63 +1,39 @@
 if(window.localStorage.grids){
-		var grids = JSON.parse(window.localStorage.getItem("grids"));
-		
-		for (let i =0 ; i<grids.length;i++) {
-			$('#grid-container > div').eq(i).find("img").attr("src", grids[i]);
-		}
-
-	console.log(grids);
-		console.log(grids.length);
+	var grids = JSON.parse(window.localStorage.getItem("grids"));
+	for (let i =0 ; i<grids.length;i++) {
+		$('#grid-container > div').eq(i).find("img").attr("src", grids[i]);
+	}
 } else {
 	var grids = [];
 	grids.length = 9;
-	console.log(grids);
-console.log(grids.length);
-
 }
-
 
 $('ol').draggable();
 $('#grid-container > div').droppable({
-  drop: function(e) {
-  console.log('drop!');
-   console.log(e);
-   console.log(e.originalEvent.target);
-   console.log(e.target);
-   console.log('------------');
-   console.log(grids);
+	drop: function(e) {
 
-   var ev = e.originalEvent;
+	var ev = e.originalEvent;
 
 	var id = $(this).attr("data-id") * 1;
 	grids[id] = ev.target.src ? e.originalEvent.target.src : e.originalEvent.target.querySelector('img').src;
-	$(this).html('<img src="'+grids[id]+'" alt="image"><div class="delete"> X </div>');
-
-	
-    console.log(e.originalEvent.target);
-    console.log(e.originalEvent.originalEvent.target.parentElement);
+	$(this).html('<img src="'+grids[id]+'" alt="image">');
 
     if(e.originalEvent.target.localName == 'img') {
-
 	   e.originalEvent.originalEvent.target.parentElement.style.top = '0px';
 	   e.originalEvent.originalEvent.target.parentElement.style.left = '0px';
-
 	} else {
 		e.originalEvent.target.style.top = '0px';
 	    e.originalEvent.target.style.left = '0px';
 	}
-	$('.delete').on('click', function(e) {
-		console.log('alert!deleting')
-		console.log(e);
-		console.log(e.target.parentElement.attributes['data-id'].value);
-		e.target.parentElement.querySelector('img').src = '';
-		grids[e.target.parentElement.attributes['data-id'].value] = '';
-	});
+	
   }
 });
-
-
 
 $('.save').on('click',function() {
 	window.localStorage.setItem('grids', JSON.stringify(grids));
 });
 
+$('.delete').on('click', function(e) {
+		e.target.parentElement.querySelector('img').src = '';
+		grids[e.target.parentElement.attributes['data-id'].value] = '';
+	});
