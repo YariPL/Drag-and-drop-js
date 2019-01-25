3d2bc5071142e3591964e7a4d2d6255cf00bcdc3
Yaroslav <yaroslav.khudchenko@gmail.com>
2019-01-25 09:52:58 +0100

    commented code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
+//when load page check if there is a version of grid in localstorage
+//and if yes grab it
 if(window.localStorage.grids){
 	var grids = JSON.parse(window.localStorage.getItem("grids"));
 	for (let i =0 ; i<grids.length;i++) {
+		//render new html for elements which has some image(based on grids array)
 		if(grids[i]){
-			console.log($('#grid-container'));
 			$('#grid-container > div').eq(i).html('<img src="'+grids[i]+'" alt=""><div class="delete"> X </div>');
 		}
 	}
 } else {
+	//if there is nothing in localstorage -> create new array with 9 instances
 	var grids = [];
 	grids.length = 9;
 }
-
+//make all 'li' elements draggable(fire event)
 $('li').draggable();
+//make all divs inside grid-container id droppable(fire event)
 $('#grid-container > div').droppable({
 	drop: function(e) {
 	var ev = e.originalEvent;
-
+	//make sure this is a number
 	var id = $(this).attr("data-id") * 1;
+	
+	//push image url to grids array
+	//here check which element user grab image itself or parent and forced taking img src in both cases
 	grids[id] = ev.target.src ? e.originalEvent.target.src : e.originalEvent.target.querySelector('img').src;
+	//render new html on drop
 	$(this).html('<img src="'+grids[id]+'" alt=""><div class="delete"> X </div>');
 
+	//reset position of original image(put it back to list)
     if(e.originalEvent.target.localName == 'img') {
 		e.originalEvent.originalEvent.target.parentElement.style.top = '0px';
 		e.originalEvent.originalEvent.target.parentElement.style.left = '0px';
@@ -27,6 +36,7 @@ $('#grid-container > div').droppable({
 		e.originalEvent.target.style.top = '0px';
 	    e.originalEvent.target.style.left = '0px';
 	}
+	//delete image from grid
 	$('.delete').on('click', function(e) {
 		console.log(e);
 		e.target.classList.add('hide');
@@ -36,11 +46,11 @@ $('#grid-container > div').droppable({
 	});
   }
 });
-
+//save to localstorage
 $('.save').on('click',function() {
 	window.localStorage.setItem('grids', JSON.stringify(grids));
 });
-
+//delete image from grid
 $('.delete').on('click', function(e) {
 	console.log(e);
 	e.target.classList.add('hide');
